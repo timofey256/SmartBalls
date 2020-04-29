dc989522664ce7f6de996b973c69a2c978a74e0e
timofey256 <shetilin.timofey@gmail.com>
2020-04-29 19:32:13 +0300

    Добавлено передвижение персонажа и радар

---
+++ Assets/Scripts/ObjectsOnScene.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsOnScene : MonoBehaviour
+{
+    
+    private Dictionary<float, string> _objectsCoordinates = new Dictionary<float, string>(); // Словарь, содержащий координату объекта и его тип
+
+    // Функция получения координат всех игроков:
+    public Dictionary<float, string> GetPlayersPosition(Vector3 myCoodinates) {
+        GameObject[] objects = GetObjectsByLayer(8); // 8 - номер слоя InteractionLayers
+
+        for (int i = 0; i < objects.Length; i++)
+        {   
+            if (myCoodinates == objects[i].transform.position)  
+            {
+                // Если координаты объекта из списка совпадают с координатами персонажа, запрашивающего объекты поблизости,
+                // то эти данные не записываются, т.к. свои данные он не запрашивает.
+            }
+            else if (objects[i].tag == "Player") 
+            {
+                _objectsCoordinates.Add(DistanceBetweenObjects(myCoodinates, objects[i].transform.position), "Character");
+            }
+            else if (objects[i].tag == "other-object") 
+            {
+                _objectsCoordinates.Add(DistanceBetweenObjects(myCoodinates, objects[i].transform.position), "other-object");
+            }
+        }
+
+        return _objectsCoordinates;
+    }
+
+    // Функция получения всех объектов, находящихся в слое InteractionLayers
+    // InteractionLayers - слой, в котором будут находится все объекты, необходимые нейронке
+    private GameObject[] GetObjectsByLayer(int layer)
+    {
+        var goArray = FindObjectsOfType(typeof(GameObject)) as GameObject[];
+        var goList = new System.Collections.Generic.List<GameObject>();
+
+        for (int i = 0; i < goArray.Length; i++)
+        {
+            if (goArray[i].layer == layer)
+            {
+                goList.Add(goArray[i]);
+            }
+        }
+
+        if (goList.Count == 0)
+        {
+            return null;
+        }
+
+        return goList.ToArray();
+    }
+
+    private float DistanceBetweenObjects(Vector3 objectCoodinates1, Vector3 objectCoodinates2) {
+        return Vector3.Distance(objectCoodinates1, objectCoodinates2);
+    } 
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+TODO:
+  - Обнаружение персонажем пропасти поблизости;
+  - 
+*/
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject player;
+
+    private CharacterController playerController;
+    private ObjectsOnScene playersOnScene;  // Скрипт получения координат других персонажей
+
+    private float _playerSpeed = 5f; // Скорость персонажа
+    private float _jumpPower = 8f;  // Высота прыжка
+    private float _gravitationForce = 4f;
+    private Vector3 _playerPosition;
+
+    void Start()
+    {
+        playersOnScene = GetComponent<ObjectsOnScene>();
+        playerController = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;   // Блокировка курсора
+    }
+
+    void Update()
+    {
+        PlayerWalking();
+        PlayerGravitation();      
+    }
+
+    // Ходьба персонажа:
+    private void PlayerWalking() 
+    {
+        _playerPosition = Vector3.zero;
+        _playerPosition.x = Input.GetAxis("Horizontal") * _playerSpeed;
+        _playerPosition.z = Input.GetAxis("Vertical") * _playerSpeed;
+
+        _playerPosition.y = _gravitationForce;  // Падение
+
+        playerController.Move(_playerPosition * Time.deltaTime);
+    }
+
+    // Гравитация персонажа:
+    private void PlayerGravitation() 
+    {
+        // Если персонаж в воздухе, то падает:
+        if (!playerController.isGrounded)
+        {
+            _gravitationForce -= 20f * Time.deltaTime;
+        }
+
+        PlayerJumping();
+    }
+
+    // Прыжок персонажа:
+    private void PlayerJumping() 
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && playerController.isGrounded)
+        {
+            _gravitationForce = _jumpPower;
+        }
+    }
+
+    // Функция, возвращающая объекты поблизости
+    private Dictionary<float, string> CheckAround() 
+    {
+        return playersOnScene.GetPlayersPosition(player.transform.position);
+    }
+}
