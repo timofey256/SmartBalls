82d932b696bec047c7fbd3c91534db8f907ca999
timofey256 <shetilin.timofey@gmail.com>
2020-04-30 11:48:06 +0300

    Обновлен скрипт ходьбы персонажа для использования вектора направления

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 /*
 TODO:
   - Обнаружение персонажем пропасти поблизости;
-  - 
+  - Переделать движение персонажа с помощью заданого вектора;
+  - Заменить Vector3.Distance();
 */
 
 public class PlayerMovement : MonoBehaviour
@@ -30,32 +31,42 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        PlayerWalking();
-        PlayerGravitation();      
+        PlayerGravitation();
+
+        Vector3 movement = new Vector3();
+        movement.x = 1;
+        movement.z = 1;    
+        
+        PlayerWalking(movement, 1.5f);  
     }
 
     // Ходьба персонажа:
-    private void PlayerWalking() 
+    private void PlayerWalking(Vector3 movementVector, float movementCoefficient) 
     {
-        _playerPosition = Vector3.zero;
-        _playerPosition.x = Input.GetAxis("Horizontal") * _playerSpeed;
-        _playerPosition.z = Input.GetAxis("Vertical") * _playerSpeed;
+        float playerDirectionX = movementVector.x;
+        float playerDirectionZ = movementVector.z;
 
-        _playerPosition.y = _gravitationForce;  // Падение
-
-        playerController.Move(_playerPosition * Time.deltaTime);
+        transform.Translate(Vector3.forward * _playerSpeed * playerDirectionZ * Time.deltaTime + Vector3.right * _playerSpeed * playerDirectionZ * Time.deltaTime);       
     }
 
     // Гравитация персонажа:
     private void PlayerGravitation() 
-    {
+    {   
+        
         // Если персонаж в воздухе, то падает:
         if (!playerController.isGrounded)
         {
             _gravitationForce -= 20f * Time.deltaTime;
         }
 
+        PlayerFalling();
         PlayerJumping();
+    }
+
+    private void PlayerFalling() {
+        _playerPosition = Vector3.zero;
+        _playerPosition.y = _gravitationForce;
+        playerController.Move(_playerPosition * Time.deltaTime);
     }
 
     // Прыжок персонажа:
